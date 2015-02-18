21413519d1b126b742c789bb01be1df3009722ec
Tim Kersey <t@k-t.im>
2015-02-18 11:07:40 -0800

    zsh

---
+++ zsh/aliases.zsh
@@ -0,0 +1,31 @@
+###########
+# GENERAL #
+###########
+alias l='ls -lah' # l for list style, a for all including hidden, h for human readable file sizes
+alias ..='cd ..' # move up 1 dir
+alias ...='cd ../..' # move up 2 dirs
+alias reload!='. ~/.zshrc'
+
+########
+# BREW #
+########
+alias b="brew"
+alias bupo="brew up && brew outdated"
+
+#######
+# Git #
+#######
+alias gti=git
+alias git=hub
+
+###############
+# DIRECTORIES #
+###############
+alias cdc="cd ~/Dropbox/code"
+alias ccl="cd ~/Dropbox/clients"
+alias cdd="cd ~/downloads"
+
+########
+# MISC #
+########
+alias bkoff="cd ~/.bin/backup && git reset HEAD --hard && git pull && chmod 755 backup.sh && /bin/bash -l -c '~/.bin/backup/backup.sh'"
---
+++ zsh/functions.zsh
@@ -0,0 +1,1 @@
+function gi() { curl -s https://www.gitignore.io/api/${(j:,:)@} ;}
