bfcb465935cb00107ece28e9c953c30a3f121a10
Tim Kersey <t@k-t.im>
2015-10-26 10:12:27 -0700

    Adds ssh-agent

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -10,4 +10,12 @@ SAVEHIST=10000
 # vim mode
 bindkey -v
 
+# ssh-agent
+SSHAGENT=/usr/bin/ssh-agent
+SSHAGENTARGS="-s"
+if [ -z "$SSH_AUTH_SOCK" -a -x "$SSHAGENT" ]; then
+eval `$SSHAGENT $SSHAGENTARGS`
+  trap "kill $SSH_AGENT_PID" 0
+fi
+
 setopt no_kshglob
