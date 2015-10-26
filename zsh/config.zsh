ed1344e22243c3e3f3b22a314a63a27d92f74ee9
Tim Kersey <t@k-t.im>
2015-10-26 14:14:02 -0700

    Sends output from eval to null

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -14,7 +14,7 @@ bindkey -v
 SSHAGENT=/usr/bin/ssh-agent
 SSHAGENTARGS="-s"
 if [ -z "$SSH_AUTH_SOCK" -a -x "$SSHAGENT" ]; then
-eval `$SSHAGENT $SSHAGENTARGS`
+eval `$SSHAGENT $SSHAGENTARGS` > /dev/null
   trap "kill $SSH_AGENT_PID" 0
 fi
 
