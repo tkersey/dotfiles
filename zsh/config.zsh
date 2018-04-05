69eea519080f1e19abc2141add2c5ebafd2f4c8f
Tim Kersey <t@k-t.im>
2018-04-05 13:03:54 -0700

    Remove unused ssh-agent

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -10,12 +10,4 @@ SAVEHIST=10000
 # vim mode
 bindkey -v
 
-# ssh-agent
-SSHAGENT=/usr/bin/ssh-agent
-SSHAGENTARGS="-s"
-if [ -z "$SSH_AUTH_SOCK" -a -x "$SSHAGENT" ]; then
-eval `$SSHAGENT $SSHAGENTARGS` > /dev/null
-  trap "kill $SSH_AGENT_PID" 0
-fi
-
 setopt no_kshglob
