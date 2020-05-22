a4cce96e70efb673763932b2add32f202501bd19
Tim Kersey <tim@carbonfive.com>
2020-05-22 13:43:19 -0700

    VSCode cli

---
+++ config/fish.symlink/functions/code.fish
@@ -0,0 +1,3 @@
+function code --wraps='/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code' --description 'alias code /Applications/Visual Studio Code.app/Contents/Resources/app/bin/code'
+  /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code $argv;
+end
