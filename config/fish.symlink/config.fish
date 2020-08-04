1d18be8b1159e12f64e678c4e1c4b467d169c3ec
Tim Kersey <tim@carbonfive.com>
2020-08-04 10:31:31 -0700

    Config for latest client

--- config/fish.symlink/config.fish
+++ config/fish.symlink/config.fish
@@ -1,14 +1,2 @@
-source /usr/local/share/chruby/chruby.fish
-source /usr/local/share/chruby/auto.fish
-
-set --export ANDROID $HOME/Library/Android;
-set --export ANDROID_HOME $ANDROID/sdk;
-set -gx PATH $ANDROID_HOME/tools $PATH;
-set -gx PATH $ANDROID_HOME/tools/bin $PATH;
-set -gx PATH $ANDROID_HOME/platform-tools $PATH;
-set -gx PATH $ANDROID_HOME/emulator $PATH
-
-set --export JAVA_HOME /Applications/Android\ Studio.app/Contents/jre/jdk/Contents/Home;
-set -gx PATH $JAVA_HOME/bin $PATH;
-
 direnv hook fish | source
+source (brew --prefix asdf)/asdf.fish
