5154bc1a96616b45aa82a3b85bc5c8c1215d5f18
Tim Kersey <t@k-t.im>
2021-06-06 12:46:26 -0700

    Fish compatible ssh-agent

--- config/fish.symlink/config.fish
+++ config/fish.symlink/config.fish
@@ -1,4 +1,11 @@
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
 
+if not pgrep -f ssh-agent > /dev/null
+  eval (ssh-agent -c)
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+end
+
 direnv hook fish | source
