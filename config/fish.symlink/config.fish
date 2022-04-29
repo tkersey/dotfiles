fa0059058e8e2c68535bbabc9df7ea6516b3925b
Tim Kersey <t@k-t.im>
2022-04-29 09:55:05 -0700

    Use command ssh-agent -s

--- config/fish.symlink/config.fish
+++ config/fish.symlink/config.fish
@@ -1,16 +1,17 @@
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
 
-if not pgrep -f ssh-agent > /dev/null
-  eval (ssh-agent -c)
-  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-end
+#if not pgrep -f ssh-agent > /dev/null
+#  eval (ssh-agent -c)
+#  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+#  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+#  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+#end
 
 direnv hook fish | source
 fish_add_path /usr/local/sbin
 
-if status is-login
-  ssh-add
-end
+#if status is-login
+#  ssh-add
+#end
+# using command ssh-agent -s instead
