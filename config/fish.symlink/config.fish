b8af4b0e0c5a6cafde7894444f434db185cec465
Tim Kersey <t@k-t.im>
2022-05-10 19:05:06 -0700

    Fix ssh agent

--- config/fish.symlink/config.fish
+++ config/fish.symlink/config.fish
@@ -1,18 +1,13 @@
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
 
-#if not pgrep -f ssh-agent > /dev/null
-#  eval (ssh-agent -c)
-#  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-#  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-#  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-#end
-
 fish_add_path /usr/local/sbin
 
-#if status is-login
-#  ssh-add
-#end
-# using command ssh-agent -s instead
+# ssh agent
+if test -f ~/.ssh/id_ed25519
+  killer ssh-agent > /dev/null 2>&1
+  eval (ssh-agent -c) > /dev/null 2>&1
+  ssh-add ~/.ssh/id_ed25519 > /dev/null 2>&1
+end
 
 direnv hook fish | source
