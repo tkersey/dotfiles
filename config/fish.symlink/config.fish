526c1e58182d7e2d88cfe85bff598636ff1c5417
Tim Kersey <t@k-t.im>
2022-06-26 16:41:44 -0700

    fish cleanup

--- config/fish.symlink/config.fish
+++ config/fish.symlink/config.fish
@@ -1,15 +1,6 @@
-source /usr/local/share/chruby/chruby.fish
-source /usr/local/share/chruby/auto.fish
-
 fish_add_path /usr/local/sbin
-
-# ssh agent
-if test -f ~/.ssh/id_ed25519
-  killer ssh-agent > /dev/null 2>&1
-  eval (ssh-agent -c) > /dev/null 2>&1
-  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-  ssh-add ~/.ssh/id_ed25519 > /dev/null 2>&1
-end
-
 direnv hook fish | source
+
+if status is-login
+  ssh-add
+end
