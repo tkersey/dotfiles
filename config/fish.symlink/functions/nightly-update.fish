bd958686fe28477c6a558341f1cb9e07171797e8
Tim Kersey <t@k-t.im>
2023-10-17 10:07:37 -0700

    Move to wezterm nightly and change update function

--- config/fish.symlink/functions/nightly-update.fish
+++ config/fish.symlink/functions/nightly-update.fish
@@ -1,5 +1,7 @@
-function nvim-nightly
+function nightly-update
   brew cleanup luajit neovim tree-sitter --prune=1
   brew uninstall --ignore-dependencies luajit neovim tree-sitter
   brew install luajit neovim tree-sitter
+
+  brew upgrade --cask wezterm-nightly --no-quarantine --greedy-latest
 end
