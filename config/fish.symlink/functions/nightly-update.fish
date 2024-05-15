f7e46eeafc245b151be6ad8be3ef0a384e5c81a3
Tim Kersey <t@k-t.im>
2024-05-15 08:42:27 -0700

    Can't get nightly wezterm to work

--- config/fish.symlink/functions/nightly-update.fish
+++ config/fish.symlink/functions/nightly-update.fish
@@ -2,6 +2,4 @@ function nightly-update
     brew cleanup luajit neovim tree-sitter --prune=1
     brew uninstall --ignore-dependencies luajit neovim tree-sitter
     brew install luajit neovim tree-sitter
-
-    brew upgrade --cask wezterm@nightly --no-quarantine --greedy-latest
 end
