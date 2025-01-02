d5523890e356390df224860e1357fb4a2275a1eb
Tim Kersey <t@k-t.im>
2025-01-01 16:48:49 -0800

    LazyVim 14 upgrade

--- config/nvim.symlink/lua/config/options.lua
+++ config/nvim.symlink/lua/config/options.lua
@@ -2,7 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.mapleader = ","
-vim.g.lazyvim_picker = "telescope"
 
 local opt = vim.opt
 
