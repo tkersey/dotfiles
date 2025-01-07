61fb2eef2a8cc792691bbbc0a54571bbc0b111be
Tim Kersey <t@k-t.im>
2025-01-07 14:03:43 -0800

    Add python_import

--- config/nvim.symlink/lua/config/options.lua
+++ config/nvim.symlink/lua/config/options.lua
@@ -2,7 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.mapleader = ","
+vim.g.lazyvim_python_lsp = "pyright"
 
-local opt = vim.opt
-
-opt.autoindent = true
+vim.opt.autoindent = true
