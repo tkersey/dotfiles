56d5ceb2e72e259a9d018232b7cb3c3d3bee979b
Tim Kersey <t@k-t.im>
2025-01-09 08:02:24 -0800

    Remove pyenv

--- config/fish.symlink/config.fish
+++ config/fish.symlink/config.fish
@@ -4,7 +4,6 @@ fish_add_path /opt/homebrew/sbin
 direnv hook fish | source
 fzf --fish | source
 zoxide init fish | source
-pyenv init - fish | source
 
 if status is-login
     ssh-add >/dev/null 2>&1
