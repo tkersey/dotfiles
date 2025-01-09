566eff85ed9c7be86f27d8029a075abdb1fd1374
Tim Kersey <t@k-t.im>
2025-01-09 10:02:25 -0800

    Update LSP for python

--- config/nvim.symlink/lua/plugins/lsp.lua
+++ config/nvim.symlink/lua/plugins/lsp.lua
@@ -3,16 +3,36 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
+
+      -- Common capabilities for all LSP servers
+      local capabilities = {
+        workspace = {
+          didChangeWatchedFiles = {
+            dynamicRegistration = true,
+          },
+        },
+      }
+
+      -- Sourcekit setup
       lspconfig.sourcekit.setup({
-        capabilities = {
-          workspace = {
-            didChangeWatchedFiles = {
-              dynamicRegistration = true,
+        capabilities = capabilities,
+      })
+
+      -- Pyright setup
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+        settings = {
+          python = {
+            analysis = {
+              typeCheckingMode = "recommended",
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
             },
           },
         },
       })
 
+      -- LSP keybindings
       vim.api.nvim_create_autocmd("LspAttach", {
         desc = "LSP Actions",
         callback = function(args)
