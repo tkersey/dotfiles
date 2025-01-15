c8e59432b8f94069f39c0f4bcce6fa39bee8a4ca
Tim Kersey <t@k-t.im>
2025-01-15 09:00:10 -0800

    Removing unnecessary config

--- config/nvim.symlink/lua/plugins/nvim-lspimport.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "stevanmilic/nvim-lspimport", -- imports only for pyright server
-  config = function()
-    vim.keymap.set("n", "<leader>li", require("lspimport").import, { noremap = true }) -- lsp import code action
-  end,
-}
