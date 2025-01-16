101ccd9b69469e23e896e04e0377152c98411aef
Tim Kersey <t@k-t.im>
2025-01-16 08:28:25 -0800

    Add ToggleTerm

---
+++ config/nvim.symlink/lua/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    opts = {
+      size = 20,
+      open_mapping = [[<leader>t]],
+      direction = "horizontal",
+    },
+    version = "*",
+  },
+}
