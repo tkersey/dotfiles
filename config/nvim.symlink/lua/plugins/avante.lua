97f7a978e7910626c220e803936f33123f101d31
Tim Kersey <t@k-t.im>
2025-02-11 17:29:20 -0800

    Removing nvim-cmp

--- config/nvim.symlink/lua/plugins/avante.lua
+++ config/nvim.symlink/lua/plugins/avante.lua
@@ -13,8 +13,6 @@ return {
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
     "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
