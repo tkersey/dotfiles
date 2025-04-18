1b970fb09cc54b835e4f235b41b3b5aa85a65df4
Tim Kersey <t@k-t.im>
2025-04-18 11:49:50 -0700

    Use snacks.picker

--- config/nvim.symlink/lua/config/options.lua
+++ config/nvim.symlink/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.mapleader = ","
+vim.g.lazyvim_picker = "snacks"
 vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.lazyvim_python_ruff = "ruff"
 
