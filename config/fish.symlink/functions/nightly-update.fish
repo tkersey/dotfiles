e80c7ca194375da4731656ab90e2b614a982dfe3
Tim Kersey <t@k-t.im>
2025-06-12 16:50:24 -0700

    Get wezterm nightly

--- config/fish.symlink/functions/nightly-update.fish
+++ config/fish.symlink/functions/nightly-update.fish
@@ -2,4 +2,5 @@ function nightly-update
     brew cleanup luajit neovim tree-sitter --prune=1
     brew uninstall --ignore-dependencies luajit neovim tree-sitter
     brew install luajit neovim tree-sitter
+    brew upgrade --cask wezterm@nightly --no-quarantine --greedy-latest
 end
