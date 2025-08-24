d864065fe02107b0376a6a793eed8d0082f2e212
Tim Kersey <t@k-t.im>
2025-08-24 08:31:03 -0700

    Used gpt-5 and Opus 4.1 to cleanup lazyvim config

--- config/nvim.symlink/lua/config/options.lua
+++ config/nvim.symlink/lua/config/options.lua
@@ -2,9 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.mapleader = ","
-vim.g.lazyvim_picker = "snacks"
 vim.g.lazyvim_python_lsp = "basedpyright"
-vim.g.lazyvim_python_ruff = "ruff"
 
-vim.opt.autoindent = true
 vim.opt.relativenumber = false
--- config/nvim.symlink/lua/plugins/lsp.lua
+++ config/nvim.symlink/lua/plugins/lsp.lua
@@ -1,77 +1,35 @@
 return {
-  { "neovim/nvim-lspconfig" },
   {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup({})
-    end,
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        sourcekit = {
+          filetypes = { "swift" },
+        },
+        basedpyright = {
+          settings = {
+            basedpyright = {
+              pythonPath = ".venv/bin/python",
+              analysis = {
+                typeCheckingMode = "off",
+                diagnosticMode = "workspace",
+              },
+            },
+          },
+        },
+      },
+    },
   },
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     config = function()
       require("mason-tool-installer").setup({
         ensure_installed = {
-          -- Formatters
-          "eslint_d",
-          "prettierd",
-          "stylua",
-          --          "ruff",
-          "shellcheck",
-          "taplo",
-
-          -- LSP servers are handled by mason-lspconfig
+          "eslint_d",   -- Fast ESLint daemon for linting
+          "prettierd",  -- Fast Prettier daemon for formatting
         },
         auto_update = true,
         run_on_start = true,
-      })
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      local lspconfig = require("lspconfig")
-
-      -- Common capabilities for all LSP servers
-      local capabilities = {
-        workspace = {
-          didChangeWatchedFiles = {
-            dynamicRegistration = true,
-          },
-        },
-      }
-
-      -- Configure Mason-LSPconfig for servers that can be installed via Mason
-      require("mason-lspconfig").setup({
-        automatic_installation = true,
-        ensure_installed = { "basedpyright" },
-      })
-
-      -- Set up the servers
-      -- Python
-      lspconfig.basedpyright.setup({
-        capabilities = capabilities,
-        autostart = true,
-        settings = {
-          basedpyright = {
-            pythonPath = ".venv/bin/python",
-            analysis = {
-              typeCheckingMode = "off", -- or "strict" if you prefer
-              diagnosticMode = "workspace",
-              useLibraryCodeForTypes = true,
-              autoImportCompletions = true,
-              reportUnusedImport = false,
-              reportMissingImports = false,
-            },
-          },
-        },
-      })
-
-      -- Swift
-      lspconfig.sourcekit.setup({
-        capabilities = capabilities,
-        autostart = true,
-        filetypes = { "swift" },
-        cmd = { "sourcekit-lsp" },
       })
     end,
   },
