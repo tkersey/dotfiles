9f754438e4cc1f69a28fcc14fc97490c8bff3bb3
Tim Kersey <t@k-t.im>
2026-04-04 20:04:21 -0700

    treesitter is no longer needed

--- fish/functions/nightly-update.fish
+++ fish/functions/nightly-update.fish
@@ -1,5 +1,5 @@
 function nightly-update
-    brew cleanup luajit neovim tree-sitter tree-sitter-cli --prune=1
-    brew uninstall --ignore-dependencies luajit neovim tree-sitter tree-sitter-cli
-    brew install luajit neovim tree-sitter tree-sitter-cli
+    brew cleanup luajit neovim tree-sitter-cli --prune=1
+    brew uninstall --ignore-dependencies luajit neovim tree-sitter-cli
+    brew install luajit neovim tree-sitter-cli
 end
